bf24e711a5d246f4008b93fd05d961432a44ba7b
Diego Achury <achurycompany@gmail.com>
2013-05-14 17:38:52 -0500

    pr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppCoco::Application.config.secret_token = '2c4e6b6ac0f339defd13d681b43cf20de5f4323257cbf636288dd9c81109cdc5d62fd0b8951a2da774744e0f973d1db47c05061eee1cf5b084ca2307ccdaa4b1'
